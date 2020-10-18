c44a92e87383821e633a67f088b7e6a04faceebb
Nazri <your@example.com>
2020-10-18 15:03:35 +0800

    Exception implementation

--- src/main/java/com0/subtracker/exception/SubscriptionException.java
+++ src/main/java/com0/subtracker/exception/SubscriptionException.java
@@ -1,2 +1,12 @@
-package com0.subtracker.exception;public class SubscriptionException {
+package com0.subtracker.exception;
+
+public class SubscriptionException extends Exception{
+
+    public SubscriptionException(String message){
+        super(message);
+    }
+
+    public SubscriptionException(Throwable throwable) {
+        super(throwable);
+    }
 }
--- src/main/java/com0/subtracker/exception/UserException.java
+++ src/main/java/com0/subtracker/exception/UserException.java
@@ -1,2 +1,12 @@
-package com0.subtracker.exception;public class UserException {
+package com0.subtracker.exception;
+
+public class UserException extends Exception{
+
+    public UserException(String message){
+        super(message);
+    }
+
+    public UserException(Throwable throwable) {
+        super(throwable);
+    }
 }
