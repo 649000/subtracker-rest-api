cf164a630f5ad40b31db5f076fe4e93de438262d
Nazri <your@example.com>
2020-10-18 15:05:08 +0800

    Corrected the primary key data type for Subscription

--- src/main/java/com0/subtracker/repository/SubscriptionRepository.java
+++ src/main/java/com0/subtracker/repository/SubscriptionRepository.java
@@ -4,6 +4,8 @@ import com0.subtracker.model.Subscription;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.UUID;
+
 @Repository
-public interface SubscriptionRepository extends CrudRepository<Subscription, String> {
+public interface SubscriptionRepository extends CrudRepository<Subscription, UUID> {
 }
