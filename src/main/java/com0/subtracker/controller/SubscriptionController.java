beacc4969b4aa3a1a45a1696cd74d7bbb169a4c5
Nazri <your@example.com>
2020-10-19 20:45:48 +0800

    Implemented SubscriptionService logic and called via SubscriptionController

--- src/main/java/com0/subtracker/controller/SubscriptionController.java
+++ src/main/java/com0/subtracker/controller/SubscriptionController.java
@@ -2,29 +2,57 @@ package com0.subtracker.controller;
 
 
 import com0.subtracker.model.Subscription;
+import com0.subtracker.service.SubscriptionService;
 import io.crnk.core.queryspec.QuerySpec;
 import io.crnk.core.repository.ResourceRepositoryBase;
 import io.crnk.core.resource.list.ResourceList;
+import lombok.SneakyThrows;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
+import javax.validation.Valid;
 import java.util.UUID;
+
 @Component
 @Slf4j
 public class SubscriptionController extends ResourceRepositoryBase<Subscription, UUID> {
-    public SubscriptionController() {
+
+    private SubscriptionService subscriptionService;
+
+    @Autowired
+    public SubscriptionController(SubscriptionService subscriptionService) {
         super(Subscription.class);
+        this.subscriptionService = subscriptionService;
     }
 
+    @SneakyThrows
+    @Override
+    public Subscription create(@Valid Subscription subscription) {
+        return subscriptionService.create(subscription);
+    }
+
+    @SneakyThrows
     @Override
     public ResourceList<Subscription> findAll(QuerySpec querySpec) {
-        log.info("findAll");
-        return null;
+        return querySpec.apply(subscriptionService.findAll());
     }
 
+    @SneakyThrows
     @Override
-    public Subscription findOne(UUID id, QuerySpec querySpec) {
-        log.info("findOne");
-        return new Subscription();
+    public Subscription findOne(@Valid UUID id, QuerySpec querySpec) {
+        return subscriptionService.findOne(id);
+    }
+
+    @SneakyThrows
+    @Override
+    public Subscription save(@Valid Subscription subscription) {
+        return subscriptionService.update(subscription);
+    }
+
+    @SneakyThrows
+    @Override
+    public void delete(@Valid UUID id) {
+        subscriptionService.delete(id);
     }
 }
--- src/main/java/com0/subtracker/service/SubscriptionService.java
+++ src/main/java/com0/subtracker/service/SubscriptionService.java
@@ -1,11 +1,19 @@
 package com0.subtracker.service;
 
+import com0.subtracker.exception.SubscriptionException;
 import com0.subtracker.model.Subscription;
+import com0.subtracker.model.User;
 import com0.subtracker.repository.SubscriptionRepository;
+import com0.subtracker.repository.UserRepository;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+import lombok.NonNull;
+
+import java.util.List;
+import java.util.Optional;
+import java.util.UUID;
 
 @Slf4j
 @Service
@@ -13,13 +21,65 @@ public class SubscriptionService {
 
     private SubscriptionRepository subscriptionRepository;
 
+    private SecurityService securityService;
+
+    private UserRepository userRepository;
+
     @Autowired
-    public SubscriptionService(SubscriptionRepository subscriptionRepository) {
+    public SubscriptionService(SubscriptionRepository subscriptionRepository, SecurityService securityService, UserRepository userRepository) {
         this.subscriptionRepository = subscriptionRepository;
+        this.securityService = securityService;
+        this.userRepository = userRepository;
+    }
+
+    /**
+     * Create a new subscription
+     * @param subscription
+     * @return
+     */
+    @Transactional
+    public Subscription create(@NonNull Subscription subscription) throws SubscriptionException {
+        User user = validUser();
+        user.getSubList().add(subscription);
+        return subscriptionRepository.save(subscription);
     }
 
     @Transactional
-    public Subscription createSubscription(Subscription subscription){
+    public Subscription findOne(@NonNull UUID id) throws SubscriptionException {
+        Optional<Subscription> subscription = subscriptionRepository.findById(id);
+        if (subscription.isPresent()){
+            return subscription.get();
+        } else {
+            log.error("Subscription ID: {} doesn't exist", id);
+            throw new SubscriptionException(String.format("Subscription ID :'%s' does not exist in database", id.toString()));
+        }
+    }
+
+    @Transactional
+    public List<Subscription> findAll() throws SubscriptionException{
+        return validUser().getSubList();
+    }
+
+    @Transactional
+    public Subscription update(@NonNull Subscription subscription) throws SubscriptionException {
+        User user = validUser();
+        user.getSubList().add(subscription);
         return subscriptionRepository.save(subscription);
+    }
+
+    @Transactional
+    public void delete(@NonNull UUID id){
+        subscriptionRepository.deleteById(id);
+    }
+
+    private User validUser() throws SubscriptionException {
+        String loggedInUserID = securityService.getUser().getUserID();
+        Optional<User> optionalUser = userRepository.findById(loggedInUserID);
+        if (optionalUser.isPresent()) {
+            return optionalUser.get();
+        } else{
+            log.error("User {} does not exist in database");
+            throw new SubscriptionException(String.format("User ID :'%s' does not exist in database", loggedInUserID));
+        }
     }
 }
