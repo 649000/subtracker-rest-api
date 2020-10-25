3f085cd158d35992449680043238c255a9e3e8bf
Nazri <your@example.com>
2020-10-25 19:49:36 +0800

    Removed unnecessary tests

--- src/test/java/com0/subtracker/SubtrackerApplicationTests.java
+++ src/test/java/com0/subtracker/SubtrackerApplicationTests.java
@@ -1,13 +1,6 @@
 package com0.subtracker;
 
-import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
-
-@SpringBootTest
 class SubtrackerApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
 
 }
