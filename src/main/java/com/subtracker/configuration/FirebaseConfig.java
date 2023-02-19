908180cd8313401ed47ad945a2984aad22c889e6
Nazri <649000@users.noreply.github.com>
2023-02-19 13:20:43 +0800

    Added Firebase Config as beans

--- src/main/java/com/subtracker/configuration/FirebaseConfig.java
+++ src/main/java/com/subtracker/configuration/FirebaseConfig.java
@@ -1,23 +1,42 @@
 package com.subtracker.configuration;
 
 import com.google.auth.oauth2.GoogleCredentials;
+import com.google.cloud.firestore.Firestore;
 import com.google.firebase.FirebaseApp;
 import com.google.firebase.FirebaseOptions;
+import com.google.firebase.cloud.FirestoreClient;
+import com.google.firebase.messaging.FirebaseMessaging;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-
 
 import java.io.IOException;
 
 @Configuration
 public class FirebaseConfig {
+    @Bean
+    public GoogleCredentials googleCredentials() {
+        try {
+            return GoogleCredentials.getApplicationDefault();
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
 
     @Bean
-    public void firebaseInit() throws IOException {
+    public FirebaseApp firebaseApp(GoogleCredentials credentials) {
         FirebaseOptions options = FirebaseOptions.builder()
-                .setCredentials(GoogleCredentials.getApplicationDefault()).build();
-        if (FirebaseApp.getApps().isEmpty()) {
-            FirebaseApp.initializeApp(options);
-        }
+                .setCredentials(credentials)
+                .build();
+        return FirebaseApp.initializeApp(options);
+    }
+
+    @Bean
+    public Firestore firestoreClient(FirebaseApp firebaseApp) {
+        return FirestoreClient.getFirestore(firebaseApp);
+    }
+
+    @Bean
+    public FirebaseMessaging firebaseMessaging(FirebaseApp firebaseApp) {
+        return FirebaseMessaging.getInstance(firebaseApp);
     }
 }
