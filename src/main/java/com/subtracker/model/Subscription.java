78b4310e2232d08227bbcff3d08cfd534d010e39
Nazri <649000@users.noreply.github.com>
2023-02-19 12:06:47 +0800

    Remodelled new Subscription class

--- src/main/java/com/subtracker/model/Subscription.java
+++ src/main/java/com/subtracker/model/Subscription.java
@@ -1,72 +1,38 @@
 package com.subtracker.model;
 
-import com.fasterxml.jackson.annotation.JsonProperty;
-import io.crnk.core.resource.annotations.JsonApiId;
-import io.crnk.core.resource.annotations.JsonApiResource;
+import com.google.firebase.database.annotations.NotNull;
 import lombok.Data;
-import org.springframework.data.annotation.CreatedDate;
-import org.springframework.data.annotation.LastModifiedDate;
-import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+import lombok.NoArgsConstructor;
 
-import javax.persistence.*;
-import javax.validation.constraints.*;
 import java.math.BigDecimal;
 import java.time.LocalDate;
 import java.util.Currency;
 import java.util.UUID;
-
 @Data
-@Entity
-@Table
-@EntityListeners(AuditingEntityListener.class)
-@JsonApiResource(type = "subscription")
+@NoArgsConstructor
 public class Subscription {
 
-    @Id
-    @GeneratedValue(strategy = GenerationType.AUTO)
-    @JsonApiId
-    @NotNull
+//    @NotNull
     private UUID subscriptionID;
 
-    @CreatedDate
-    @JsonProperty
-    @NotNull
-    @Column(nullable = false)
+//    @NotNull
     private LocalDate createdDate;
 
-    @LastModifiedDate
-    @JsonProperty
-    @Column
+//    @NotNull
     private LocalDate modifiedDate;
 
-    @JsonProperty
-    @NotNull
-    @NotBlank
-    @NotEmpty
-    @Column(nullable = false)
-    private String name;
+//    @NotNull
+    private String serviceName;
 
-    @JsonProperty
-    @NotNull
-    @Column(nullable = false)
+//    @NotNull
     private Currency currency;
 
-    @JsonProperty
-    @NotNull
-    @PositiveOrZero
-    @Digits(integer = 10, fraction = 2)
-    @Column
-    private BigDecimal amount;
+//    @NotNull
+    private BigDecimal monthlyAmount;
+    private BigDecimal yearlyAmount;
 
-    //In days: 1 = 1 day, 30 = 1 month
-    @JsonProperty
-    @NotNull
-    @PositiveOrZero
-    @Column(nullable = false)
-    private int cycle;
+    // In months
+    private double contractualPeriod;
 
-    @JsonProperty
-    @Column
-    private String information;
-
+    private String notes;
 }
