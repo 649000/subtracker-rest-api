0ffef1f40c45f3ee358d1be4ac2f808859e015d7
Nazri <649000@users.noreply.github.com>
2024-02-03 18:27:54 +0800

    Updated Firebase Credentials to read base64

--- src/main/java/com/subtracker/configuration/FirebaseConfig.java
+++ src/main/java/com/subtracker/configuration/FirebaseConfig.java
@@ -9,14 +9,22 @@ import com.google.firebase.messaging.FirebaseMessaging;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+import java.io.ByteArrayInputStream;
 import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+import java.util.Base64;
 
 @Configuration
 public class FirebaseConfig {
     @Bean
     public GoogleCredentials googleCredentials() {
         try {
-            return GoogleCredentials.getApplicationDefault();
+            String base64Key = System.getenv("GOOGLE_APPLICATION_CREDENTIALS_BASE64");
+            byte[] decodedKey = Base64.getDecoder().decode(base64Key);
+            return GoogleCredentials.fromStream(new ByteArrayInputStream(decodedKey));
+            // Below only works if GOOGLE_APPLICATION_CREDENTIALS is set as a file path
+            // Not possible to upload JSON file to Render/Other Cloud Providers
+//            return GoogleCredentials.getApplicationDefault();
         } catch (IOException e) {
             throw new RuntimeException(e);
         }
