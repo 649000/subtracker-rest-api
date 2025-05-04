6bdaeaea87088f6eb538a7bd3eef7106df19f276
Nazri <649000@users.noreply.github.com>
2025-05-04 14:41:37 +0800

    Updated Subscription.java model

--- src/main/java/com/subtracker/model/Subscription.java
+++ src/main/java/com/subtracker/model/Subscription.java
@@ -1,15 +1,19 @@
 package com.subtracker.model;
 
+import com.google.cloud.firestore.annotation.DocumentId;
+import com.google.cloud.spring.data.firestore.Document;
 import com.google.firebase.database.annotations.NotNull;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
 import java.util.Date;
 
+@Document(collectionName = "subscription")
 @Data
 @NoArgsConstructor
 public class Subscription {
 
+    @DocumentId
     private String subscriptionId;
 
     @NotNull
