1dc667bf46e31f9c8c1a8092d179fafd4b801955
Nazri <649000@users.noreply.github.com>
2025-05-04 17:50:35 +0800

    Fix minor bugs in UserController and UserService

--- src/main/java/com/subtracker/service/UserService.java
+++ src/main/java/com/subtracker/service/UserService.java
@@ -7,6 +7,9 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.oauth2.jwt.Jwt;
 import org.springframework.stereotype.Service;
 
+import java.util.ArrayList;
+import java.util.Date;
+
 @Service
 @Slf4j
 public class UserService {
@@ -24,6 +27,9 @@ public class UserService {
         user.setEmail(jwt.getClaimAsString("email"));
 //        TBD
 //        user.setCountry(request.getCountry());
+        user.setCreatedDate(new Date());
+        user.setModifiedDate(new Date());
+        user.setSubscriptionList(new ArrayList<>());
         return userRepository.save(user);
     }
 }
