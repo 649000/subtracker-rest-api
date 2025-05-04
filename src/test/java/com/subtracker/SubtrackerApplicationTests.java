e3bc0d77e17b330e33a68114e81c08d922c87d57
649000 <649000@users.noreply.github.com>
2025-05-04 21:18:43 +0800

    Updated pom and unit test for jacoco

--- src/test/java/com/subtracker/SubtrackerApplicationTests.java
+++ src/test/java/com/subtracker/SubtrackerApplicationTests.java
@@ -1,6 +1,14 @@
 package com.subtracker;
 
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
 class SubtrackerApplicationTests {
 
+    @Test
+    public void test() {
+        assertTrue(true);
+    }
 
 }
