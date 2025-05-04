bbbaa87b7f659db3c16ed88f30340fc414ba800b
Nazri <649000@users.noreply.github.com>
2025-05-04 17:50:59 +0800

    Rewrote SubscriptionService to use SubscriptionRepository

--- src/main/java/com/subtracker/service/SubscriptionService.java
+++ src/main/java/com/subtracker/service/SubscriptionService.java
@@ -1,97 +1,70 @@
 package com.subtracker.service;
 
-import com.google.api.core.ApiFuture;
-import com.google.cloud.firestore.*;
+import com.subtracker.exception.SubscriptionException;
 import com.subtracker.model.Subscription;
+import com.subtracker.model.User;
+import com.subtracker.repository.SubscriptionRepository;
+import com.subtracker.repository.UserRepository;
 import lombok.NonNull;
 import lombok.extern.slf4j.Slf4j;
-import org.apache.commons.lang3.ObjectUtils;
-import org.apache.commons.lang3.StringUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-import java.util.Date;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
-import java.util.concurrent.ExecutionException;
-import java.util.stream.Collectors;
+import java.util.Collections;
+import java.util.Optional;
 
 @Service
 @Slf4j
 public class SubscriptionService {
 
 
-//    private Firestore firestore;
-//
-//    private final String SUBSCRIPTION_TABLE = "subscription";
-//
-//    @Autowired
-//    public SubscriptionService(Firestore firestore) {
-//        this.firestore = firestore;
-//    }
-//
-//    public Subscription createSubscription(@NonNull Subscription subscription) throws ExecutionException, InterruptedException {
-//        ApiFuture<DocumentReference> addedDocRef = firestore.collection(SUBSCRIPTION_TABLE).add(subscription);
-//        subscription.setSubscriptionId(addedDocRef.get().getId());
-//        return subscription;
-//    }
-//
-//    public Iterable<Subscription> getSubscriptions() throws ExecutionException, InterruptedException {
-//        ApiFuture<QuerySnapshot> future = firestore.collection(SUBSCRIPTION_TABLE).get();
-//        List<QueryDocumentSnapshot> documents = future.get().getDocuments();
-//        List<Subscription> subscriptionList = documents.stream().map(doc -> {
-//                    Subscription subscription = doc.toObject(Subscription.class);
-//                    subscription.setSubscriptionId(doc.getId());
-//                    return subscription;
-//                }
-//        ).collect(Collectors.toList());
-//        return subscriptionList;
-//    }
-//
-//    public Subscription getSubscription(@NonNull final String subscriptionId) throws ExecutionException, InterruptedException {
-//        DocumentReference docRef = firestore.collection(SUBSCRIPTION_TABLE).document(subscriptionId);
-//        ApiFuture<DocumentSnapshot> future = docRef.get();
-//        DocumentSnapshot document = future.get();
-//        Subscription subscription = document.toObject(Subscription.class);
-//        subscription.setSubscriptionId(document.getId());
-//        return subscription;
-//    }
-//
-//    public void deleteSubscription(@NonNull final String subscriptionId) {
-//        ApiFuture<WriteResult> writeResult = firestore.collection(SUBSCRIPTION_TABLE).document(subscriptionId).delete();
-//    }
-//
-//    public Subscription updateSubscription(@NonNull final String subscriptionId, @NonNull final Subscription request) throws ExecutionException, InterruptedException {
-//        DocumentReference docRef = firestore.collection(SUBSCRIPTION_TABLE).document(subscriptionId);
-//        Map<String, Object> subUpdates = new HashMap<>();
-//        subUpdates.put("modifiedDate", new Date());
-//
-//        if (StringUtils.isNotEmpty(request.getCurrency())) {
-//            subUpdates.put("currency", request.getCurrency());
-//        }
-//
-//        if (StringUtils.isNotEmpty(request.getServiceName())) {
-//            subUpdates.put("serviceName", request.getServiceName());
-//        }
-//
-//        if (ObjectUtils.isNotEmpty(request.getMonthlyAmount())) {
-//            subUpdates.put("monthlyAmount", request.getMonthlyAmount());
-//        }
-//
-//        if (ObjectUtils.isNotEmpty(request.getYearlyAmount())) {
-//            subUpdates.put("yearlyAmount", request.getYearlyAmount());
-//        }
-//
-//        if (ObjectUtils.isNotEmpty(request.getContractualPeriod())) {
-//            subUpdates.put("contractualPeriod", request.getContractualPeriod());
-//        }
-//
-//        if (ObjectUtils.isNotEmpty(request.getNotes())) {
-//            subUpdates.put("notes", request.getNotes());
-//        }
-//
-//        docRef.update(subUpdates);
-//        return request;
-//    }
+    private SubscriptionRepository subscriptionRepository;
+    private UserRepository userRepository;
+
+    @Autowired
+    public SubscriptionService(SubscriptionRepository subscriptionRepository, UserRepository userRepository) {
+        this.subscriptionRepository = subscriptionRepository;
+        this.userRepository = userRepository;
+    }
+
+    //TODO: Atomic Transaction
+    public Subscription createSubscription(@NonNull Subscription subscription) {
+        User user = userRepository.findById(subscription.getUserId())
+                .orElseThrow(() -> new IllegalArgumentException("User not found: " + subscription.getUserId()));
+
+        Subscription savedSubscription = subscriptionRepository.save(subscription);
+
+        user.getSubscriptionList().add(savedSubscription.getSubscriptionId());
+        userRepository.save(user);
+
+        return savedSubscription;
+    }
+
+    public Iterable<Subscription> getSubscriptions(@NonNull final String userId) {
+        return userRepository.findById(userId)
+                .map(user -> subscriptionRepository.findAllById(user.getSubscriptionList()))
+                .orElse(Collections.emptyList());
+    }
+
+    public Subscription getSubscription(@NonNull final String subscriptionId, @NonNull final String userId) throws SubscriptionException {
+        return subscriptionRepository.findById(subscriptionId)
+                .filter(subscription -> userId.equals(subscription.getUserId()))
+                .orElseThrow(() -> new SubscriptionException("Subscription does not exist or does not belong to user"));
+    }
+
+    //TODO: Atomic Transaction
+    public void deleteSubscription(@NonNull final String subscriptionId, @NonNull final String userId) {
+        Optional<Subscription> subscriptionOpt = subscriptionRepository.findById(subscriptionId);
+
+        if (subscriptionOpt.isEmpty() || !userId.equals(subscriptionOpt.get().getUserId())) {
+            throw new SecurityException("Subscription does not exist or does not belong to user");
+        }
+
+        subscriptionRepository.deleteById(subscriptionId);
+
+        userRepository.findById(userId).ifPresent(user -> {
+            user.getSubscriptionList().remove(subscriptionId);
+            userRepository.save(user);
+        });
+    }
 }
