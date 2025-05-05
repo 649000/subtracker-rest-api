56de1061e22bc02df2cc1d6808cf80bab295c422
Nazri <649000@users.noreply.github.com>
2025-05-05 10:44:00 +0800

    Added comments for reactive repositories

--- src/main/java/com/subtracker/repository/ReactiveSubscriptionRepository.java
+++ src/main/java/com/subtracker/repository/ReactiveSubscriptionRepository.java
@@ -4,6 +4,10 @@ import com.google.cloud.spring.data.firestore.FirestoreReactiveRepository;
 import com.subtracker.model.Subscription;
 import org.springframework.stereotype.Repository;
 
+/**
+ * Reactive Firestore repository for Subscription documents.
+ * Provides asynchronous access to subscription data.
+ */
 @Repository
 public interface ReactiveSubscriptionRepository extends FirestoreReactiveRepository<Subscription> {
 }
--- src/main/java/com/subtracker/repository/ReactiveUserRepository.java
+++ src/main/java/com/subtracker/repository/ReactiveUserRepository.java
@@ -4,6 +4,10 @@ import com.google.cloud.spring.data.firestore.FirestoreReactiveRepository;
 import com.subtracker.model.User;
 import org.springframework.stereotype.Repository;
 
+/**
+ * Reactive Firestore repository for User documents.
+ * Provides asynchronous access to user data.
+ */
 @Repository
 public interface ReactiveUserRepository extends FirestoreReactiveRepository<User> {
 }
